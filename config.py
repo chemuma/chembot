2abc71338aa7a031502593987366c8c38c9bf25b
chemuma <engchemuma@gmail.com>
2025-11-03 12:54:12 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,7 @@
 # config.py
-# تمام اطلاعات حساس و تنظیمات ربات
-
-# Bot configuration
 BOT_TOKEN = "7996022698:AAG65GXEjbDbgMGFVT9ExeGFmkvj0UDqbXE"
 CHANNEL_ID = "@chemical_eng_uma"
 OPERATOR_GROUP_ID = -1002574996302
-ADMIN_IDS = [5701423397, 158893761]  # ادمین‌های اصلی
+ADMIN_IDS = [5701423397, 158893761]
 CARD_NUMBER = "6219-8619-2120-2437"
 DB_PATH = "chemeng_bot.db"
-
-# Feedback system config
-FEEDBACK_WINDOW_DAYS = 2  # چند روز کاربران فرصت امتیازدهی دارند
