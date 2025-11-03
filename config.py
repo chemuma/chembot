58d1d57fa859234b5ea73b5c164ff5a5cdb0a234
chemuma <engchemuma@gmail.com>
2025-11-03 13:00:14 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 # config.py
+
 BOT_TOKEN = "7996022698:AAG65GXEjbDbgMGFVT9ExeGFmkvj0UDqbXE"
 CHANNEL_ID = "@chemical_eng_uma"
 OPERATOR_GROUP_ID = -1002574996302
 ADMIN_IDS = [5701423397, 158893761]
 CARD_NUMBER = "6219-8619-2120-2437"
 DB_PATH = "chemeng_bot.db"
+
+# تنظیمات سیستم امتیازدهی
+FEEDBACK_DURATION_HOURS = 48  # مهلت امتیازدهی: 48 ساعت
