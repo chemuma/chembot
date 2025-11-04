02d6b53f7657e1ad1ee7e5bc9854c7e96744d2ec
chemuma <engchemuma@gmail.com>
2025-11-04 11:18:53 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # config.py
+import os
 
-BOT_TOKEN = "7996022698:AAG65GXEjbDbgMGFVT9ExeGFmkvj0UDqbXE"
-CHANNEL_ID = "@chemical_eng_uma"
-OPERATOR_GROUP_ID = -1002574996302
-ADMIN_IDS = [5701423397, 158893761]
-CARD_NUMBER = "6219-8619-2120-2437"
-DB_PATH = "chemeng_bot.db"
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7996022698:AAG65GXEjbDbgMGFVT9ExeGFmkvj0UDqbXE")
+CHANNEL_ID = os.getenv("CHANNEL_ID", "@chemical_eng_uma")
+OPERATOR_GROUP_ID = int(os.getenv("OPERATOR_GROUP_ID", "-1002574996302"))
+ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "5701423397,158893761").split(",")]
+CARD_NUMBER = os.getenv("CARD_NUMBER", "6219-8619-2120-2437")
+DB_PATH = "/app/data/chemeng_bot.db" 
 
-# تنظیمات سیستم امتیازدهی
 FEEDBACK_DURATION_HOURS = 48  # مهلت امتیازدهی: 48 ساعت
