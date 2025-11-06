2fb1f68649e601ddb3b57fa519385a753457721a
chemuma <engchemuma@gmail.com>
2025-11-06 18:43:41 +0330

    Update main.py

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ async def main() -> None:
     application.add_handler(MessageHandler(filters.Regex("^دوره‌ها/بازدیدها$"), show_events))
     application.add_handler(MessageHandler(filters.Regex("^سوالات متداول$"), faq))
     application.add_handler(MessageHandler(filters.Regex("^لغو/شروع دوباره$"), reset_bot))
-    application.add_handler(MessageHandler(filters.Regex("^ارتباط با پشتیبانی$"), lambda u, c: u.message.reply_text("پیام خود را بنویسید:")))
+   
 
     # Profile
     application.add_handler(profile_conv)
@@ -108,6 +108,7 @@ async def main() -> None:
     application.add_handler(CommandHandler("manual_reg", manual_registration))
 
     # Support & Back
+    application.add_handler(MessageHandler(filters.Regex("^ارتباط با پشتیبانی$"), lambda u, c: u.message.reply_text("پیام خود را بنویسید:")))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND & filters.ChatType.PRIVATE, handle_support_message))
     application.add_handler(MessageHandler(filters.Regex("^بازگشت$"), back_to_main_menu))
 
